7c53d03fa5ab1f1ddc615ad32baff4993690616f
Elchin <bayramlielcin03@gmail.com>
2025-05-18 05:28:37 +0400

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,26 @@
+# print('What is color of the milk?','Do you know?')
+# print('White')
+
+# This is comment
+""" Bla bla   
+bafdl
+sdfsd
+sfs
+sfdsf"""
+# a = 5 # This is also comment
+# a = "Hello world" # bla bla bla
+# print(a)
+# print(8 ** 2)
+# dəyişən = 5
+# print(dəyişən)
+
+a1 = int(input('Enter the first number: '))
+a2 = int(input('Enter the second number: '))
+c = a1 + a2
+# a1 = '21'
+#type()
+#int
+# print(type(a1))
+# print('Entered value: ',a1)
+
+print(a1,'+',a2,'=', c)
